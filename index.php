fbaacde7863dbe1e7bb65795aed1ec8cae6829a5
Paulo Roberto <psface1@gmail.com>
2015-09-30 15:47:14 -0300

    add navbar and fix header xs

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
 	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/vendors/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <style type="text/css">
+		@font-face {
+	  font-family: 'Glyphicons Halflings';
+	  src: url(fonts/bootstrap/glyphicons-halflings-regular.eot?1430235042);
+	  src: url(fonts/bootstrap/glyphicons-halflings-regular.eot?&1430235042#iefix) format("embedded-opentype"), url(fonts/bootstrap/glyphicons-halflings-regular.woff2?1430235042) format("woff2"), url(fonts/bootstrap/glyphicons-halflings-regular.woff?1430235042) format("woff"), url(fonts/bootstrap/glyphicons-halflings-regular.ttf?1430235042) format("truetype"), url(fonts/bootstrap/glyphicons-halflings-regular.svg?1430235042#glyphicons_halflingsregular) format("svg");
+	}
+   </style>
 </head>
 <body>
 	<header class="container">
@@ -19,14 +26,39 @@
 			<div id="search-container">
 				 <div id="search-container__form">		
 				    <div class="input-group">
-				      <input type="text" class="form-control" placeholder="Digite sua busca...">
+					 <input type="text" class="form-control" placeholder="Digite sua busca...">
 				      <span class="input-group-btn">
-				        <button class="btn btn-default" type="button">Buscar</button>
+				        <button class="btn btn-default hidden-xs" type="button">Buscar</button>
 				      </span>
 				    </div><!-- /input-group -->
 				</div>
 			</div>
 		</div>
 	</header>
+<nav class="navbar navbar-default navbar-static-top">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="container">	
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-film" aria-hidden="true"></span>Filmes</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Contato</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Login</a></li>        
+      </ul>
+  </div>
+    </div><!-- /.navbar-collapse -->
+</nav>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
