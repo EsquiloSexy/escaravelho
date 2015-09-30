f97eac4dfd2c0753e286affd7a27502fc76bedee
Paulo Roberto <psface1@gmail.com>
2015-09-30 00:29:00 -0300

    add header

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Escaralho - escaralhando os movies</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/vendors/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header class="container">
+		<div class="content">
+			<div id="logo-container">
+			   <div id="logo-container__logo">		
+				<a href="#"><img class="img-responsive" src="imgs/logo2.jpg" alt="Logo" title="Escaravelho"/></a>
+				</div>	
+			</div>
+			<div id="search-container">
+				 <div id="search-container__form">		
+				    <div class="input-group">
+				      <input type="text" class="form-control" placeholder="Digite sua busca...">
+				      <span class="input-group-btn">
+				        <button class="btn btn-default" type="button">Buscar</button>
+				      </span>
+				    </div><!-- /input-group -->
+				</div>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
