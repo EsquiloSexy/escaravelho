fa050f04eaccf0eb9bd5cda9b3daa404c951518e
Paulo Roberto <psface1@gmail.com>
2015-10-08 22:58:26 -0300

    Inicio pagina post, add box post, box ads(publicidade) e inicio box comentarios

---
+++ post.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Escaravelho</title>
+	<link rel="icon" href="imgs/favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/vendors/bootstrap/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/vendors/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/vendors/owl-carousel/owl.theme.css">
+	<link rel="stylesheet" type="text/css" href="css/vendors/social/arthref.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <style type="text/css">
+		@font-face {
+	  font-family: 'Glyphicons Halflings';
+	  src: url(fonts/bootstrap/glyphicons-halflings-regular.eot?1430235042);
+	  src: url(fonts/bootstrap/glyphicons-halflings-regular.eot?&1430235042#iefix) format("embedded-opentype"), url(fonts/bootstrap/glyphicons-halflings-regular.woff2?1430235042) format("woff2"), url(fonts/bootstrap/glyphicons-halflings-regular.woff?1430235042) format("woff"), url(fonts/bootstrap/glyphicons-halflings-regular.ttf?1430235042) format("truetype"), url(fonts/bootstrap/glyphicons-halflings-regular.svg?1430235042#glyphicons_halflingsregular) format("svg");
+	}
+   </style>
+</head>
+<body>
+	<header class="container">
+		<div class="content">
+			<div id="logo-container">
+			   <div id="logo-container__logo">		
+				<a href="#"><img class="img-responsive" src="imgs/logo.jpg" alt="Logo" title="Escaravelho"/></a>
+				</div>	
+			</div>
+			<div id="search-container">
+				 <div id="search-container__form">		
+				    <div class="input-group">
+					 <input type="text" class="form-control" placeholder="Digite sua busca...">
+				      <span class="input-group-btn">
+				        <button class="btn btn-default hidden-xs" type="button">Buscar</button>
+				      </span>
+				    </div><!-- /input-group -->
+				</div>
+			</div>
+		</div>
+	</header>
+
+<nav class="navbar navbar-default navbar-static-top">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="container">	
+      <ul class="nav navbar-nav">
+        <li><a href="#home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-film" aria-hidden="true"></span>Filmes</a></li>
+        <li class="li_click"><a href="#" data-toggle="modal" data-target="#contato"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Contato</a></li>
+        <li class="li_click"><a href="#" data-toggle="modal" data-target="#login"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Login</a></li>        
+      </ul>
+  </div>
+    </div><!-- /.navbar-collapse -->
+</nav>
+
+<div class="container">
+	<div class="content">
+		<section id="post">
+			<div id="header">Titulo da postagem<span id="icons"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 145 <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> 145</span></div>
+      <div id="content-post">
+        lalalal
+      </div>
+		</section>
+    <section id="ads">
+      PUBLICIDADE
+    </section>
+    <section id="comentarios">
+      <div id="header_post"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> Comentários</div>
+        <div id="content_comentarios">
+          <div class="comentario">
+            <img src="imgs/default.jpg">
+            <p>Paulo</p>
+          </div>
+        </div>
+    </div>
+	</div>
+</div>
+
+<footer>
+	<div class="container">
+	  <div class="content">	
+	 <div id="widgets"> 	
+	  <div class="footer_widget">		
+		<h1>SOBRE NÓS</h1>
+		<span class="text_footer_widget">Nós somos uma empresa que presta serviços e fala sobre cinema de forma geral, algum texto, algum texto algum</span><br>
+		<button class="btn btn-default" type="button">Saiba+</button>
+		</div>
+		<div class="footer_widget offset">		
+		<h1>SERVIÇOS</h1>
+		<span class="text_footer_widget">
+			<ul>
+				<li><a href="#">Compre ingressos</a></li>
+				<li><a href="#">Cinemas</a></li>
+				<li><a href="#">Nossa loja</a></li>
+				<li><a href="#">Promoções</a></li>
+			</ul>
+		</span>
+		</div>
+		<div class="footer_widget offset">		
+		<h1>INFORMAÇÕES</h1>
+		<span class="text_footer_widget">
+			<ul id="info">
+				<li><a href="#">Crie sua conta</a></li>
+				<li><a href="#">Fale conosco</a></li>
+				<li><a class="btsocial" href="javascript:void(0)"><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Siga-nos</a></li>
+				<li><a href="#">Parceiros</a></li>
+			</ul>
+		</span>
+		</div>
+		</div>
+		
+  </div>	
+ </div>
+ <div id="social_media">
+ <a href="#">TODOS DIREITOS RESERVADOS - ESCARAVELHO CORPORATION</a>	
+ </div>
+</footer>
+
+<div class="modal fade" id="contato" tabindex="-1" role="dialog" aria-labelledby="contato">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Contato</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="control-label">Nome:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+           <div class="form-group">
+            <label for="recipient-name" class="control-label">E-mail:</label>
+            <input type="email" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="control-label">Mensagem:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="login">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Logar</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="control-label">E-mail:</label>
+            <input type="email" class="form-control" id="recipient-name">
+          </div>
+           <div class="form-group">
+            <label for="recipient-name" class="control-label">Senha:</label>
+            <input type="password" class="form-control" id="recipient-name">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+      	<button type="button" id="btn-register" class="btn btn-primary">Registrar</button>
+        <button type="button" class="btn btn-primary">Logar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript" src="js/vendors/jquery.js"></script>
+<script type="text/javascript" src="js/vendors/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/vendors/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/vendors/socialProfiles.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
